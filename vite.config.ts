0c172ed4af76f220a50d3ad92883ebfadccc83cf
muradmammadov <murad.mammadov@nys.az>
2025-06-17 23:23:33 +0400

    fixed conflict issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,24 +5,4 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'vendor_react'
-            }
-            if (id.includes('antd')) {
-              return 'vendor_antd'
-            }
-            // digər paketləri qruplaşdırmaq üçün
-            return 'vendor'
-          }
-        }
-      }
-    },
-    // opsional olaraq xəbərdarlıq limitini artırmaq üçün
-    chunkSizeWarningLimit: 1000, // 1 MB limit qoyulur, istəsən daha da artır
-  }
 })
