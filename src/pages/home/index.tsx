ffae4c3a65c8ba2c289e5b238e2557607cafcada
muradmammadov <murad.mammadov@nys.az>
2025-09-10 11:13:58 +0400

    new function for categoris to split the title two areas title and desc

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -52,23 +52,37 @@ export default function StartTestPage() {
         </div>
       ) : (
         <div className="h-full grid lg:grid-cols-4 grid-cols-2 items-center gap-2">
-          {categories?.map((item: any, key) => (
-            <div
-              key={key}
-              className="bg-white shadow-2xl rounded-2xl p-8 max-w-lg w-full text-center space-y-6"
-            >
-              <h4 className="text-3xl font-bold text-gray-800">{item.title}</h4>
+          {categories?.map((item: any, key) => {
+            let title = item.title;
+            let desc = "";
 
-              <Button
-                type="primary"
-                size="large"
-                className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-5 text-lg rounded-xl"
-                onClick={() => confirmStartQuiz(item.id)}
+            // ___ varsa ayırırıq
+            if (item.title.includes("___")) {
+              const parts = item.title.split("___");
+              title = parts[0].trim();
+              desc = parts[1].trim();
+            }
+
+            return (
+              <div
+                key={key}
+                className="bg-white shadow-2xl rounded-2xl p-8 max-w-lg w-full text-center space-y-6"
               >
-                Start Quiz
-              </Button>
-            </div>
-          ))}
+                <h4 className="text-3xl font-bold text-gray-800">{title}</h4>
+                {desc && <p className="text-gray-600 text-base italic">{desc}</p>}
+
+                <Button
+                  type="primary"
+                  size="large"
+                  className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-5 text-lg rounded-xl"
+                  onClick={() => confirmStartQuiz(item.id)}
+                >
+                  Start Quiz
+                </Button>
+              </div>
+            );
+          })}
+
         </div>
       )}
     </div>
